baea08b9bd87ca466460b3db3cf9f160c4fc8193
aralids <dilara.sarach@abv.bg>
2024-09-17 03:27:08 -0400

    file upload via index.html

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route("/process_json", methods=["POST"])
+def process_json():
+    f = request.files["json_file"].read()
+    data = json.loads(f)
+    print("request: ", request.files["json_file"].filename)
+    return Response("Successful", status=201, mimetype='text/plain')
 
 if __name__ == '__main__':
   app.run(port=5000)

f38d0bc1cf5ffe3eec08bb26d0e2531ba5a5fe50
aralids <dilara.sarach@abv.bg>
2024-10-22 11:44:24 -0400

    minor changes to file system

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
-from flask import Flask, render_template, request, Response
+from fastapi import FastAPI, Response, Request, File, UploadFile, BackgroundTasks
+from fastapi.responses import HTMLResponse, FileResponse, StreamingResponse
+
 import json
-import sys
+from io import BytesIO
 import math
+from datetime import datetime
+
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
+
+
 from drawing_functions import draw_front_page, draw_3_slip_prints
+from config import use_mapping
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+@app.get("/")
+async def root():
+    html_content = """
+    	<!DOCTYPE html>
+	<html>
+	  <head>
+	    <title>Hello World</title>
+	  </head>
+	  <body>
+	    <h1>Hello World!</h1>
+	    <form method="post" enctype="multipart/form-data" action="/process_json">
+	    <p>
+		<label>Add file (single): </label><br/>
+		<input type="file" name="json_file"/>
+	    </p>
+	    <p>
+		<input type="submit"/>
+	    </p>
+	</form>
+	  </body>
+	</html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
-@app.route("/process_json", methods=["POST"])
-def process_json():
-
+@app.post("/process_json")
+async def process_json(request: Request, background_tasks: BackgroundTasks):
+    form = await request.form()
+    #filename = form['json_file'].filename 
+    
     # Load the JSON file.
-    f = request.files["json_file"].read()
+    f = await form['json_file'].read()
     data = json.loads(f)
     
     # Generate an empty PDF file in the "output" folder with 
     # the same name as the JSON file in the "input" folder.
-    c = canvas.Canvas("./output/" + request.files["json_file"].filename[:-5] + ".pdf", pagesize=A4, bottomup=0)
+    bfr = BytesIO()
+    c = canvas.Canvas(bfr, pagesize=A4, bottomup=0)
     
     # Draw the front page.
-    draw_front_page(c, "zbbskwug2", "06-08-2024 12:05")
+    group = data[0]["print_group_name"]
+    service_point = use_mapping["service_points"][group]
+    layout = use_mapping["layouts"][group]
+    now = datetime.now()
+    timestamp = now.strftime("%d-%m-%Y %H:%M")
+    draw_front_page(c, " ".join(group.split(" ")[1:]), timestamp)
     c.showPage()
     
     # Sorting.
@@ -42,12 +77,14 @@ def process_json():
     	if (i + 2*page_num) <= (len(data) - 1):
     	    json_obj3 = data[i+2*page_num]
     	    
-    	draw_3_slip_prints(c, json_obj1, json_obj2, json_obj3)
+    	draw_3_slip_prints(c, service_point, layout, json_obj1, json_obj2, json_obj3)
     	c.showPage()
     	
     c.save()
     
-    return Response("Successful", status=201, mimetype='text/plain')
-
-if __name__ == '__main__':
-  app.run(port=5000)
+    background_tasks.add_task(bfr.close)
+    pdf = bfr.getvalue()
+    
+    
+    headers = {'Content-Disposition': 'attachment; filename="output.pdf"'}
+    return Response(pdf, headers=headers, media_type='application/pdf')
